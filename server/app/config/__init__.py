4431387005abfb135c1b0152796e53642b26bd45
nicoladima63 <150719135+nicoladima63@users.noreply.github.com>
2025-06-26 12:32:20 +0200

    Update __init__.py

--- server/app/config/__init__.py
+++ server/app/config/__init__.py
@@ -1,2 +1,25 @@
 # app/config/__init__.py
 
+# Importa tutte le costanti principali
+from .constants import *
+
+# Se in futuro dividerai le configurazioni in moduli specifici, puoi importarli qui:
+# from .google import GOOGLE
+# from .twilio import TWILIO
+# from .paths import PATHS_DBF, PATH_APPUNTAMENTI_DBF, PATH_ANAGRAFICA_DBF
+
+# --- Facoltativo: centralizzazione di un dizionario unico ---
+# Ad esempio per accedere a tutte le costanti configurabili come oggetti
+# CONFIG = {
+#     "twilio": TWILIO,
+#     "google": GOOGLE,
+#     "medici": MEDICI,
+#     ...
+# }
+
+# Se vuoi evitare import * (più esplicito):
+# __all__ = [
+#     "COLONNE", "TIPI_APPUNTAMENTO", "TIPO_RICHIAMI", "COLORI_APPUNTAMENTO",
+#     "GOOGLE_COLOR_MAP", "MEDICI", "TWILIO", "GOOGLE",
+#     "PATH_APPUNTAMENTI_DBF", "PATH_ANAGRAFICA_DBF"
+# ]
