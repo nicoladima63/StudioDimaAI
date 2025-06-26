8f2a1fc2bdb679d81689a2d5224f81f3511b34ac
nicoladima63 <150719135+nicoladima63@users.noreply.github.com>
2025-06-26 13:11:28 +0200

    Update logging.py

--- server/app/config/logging.py
+++ server/app/config/logging.py
@@ -1,13 +1,18 @@
-# app/config/logging.py
 import logging
+import sys
 
-logger = logging.getLogger("studio_dima")
-logger.setLevel(logging.DEBUG)  # Puoi cambiare in INFO in produzione
+def setup_logging(level=logging.INFO):
+    logger = logging.getLogger()  # root logger
+    if logger.hasHandlers():
+        logger.handlers.clear()
 
-handler = logging.StreamHandler()
-formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
+    formatter = logging.Formatter(
+        fmt='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-if not logger.hasHandlers():
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
 
+    logger.setLevel(level)
