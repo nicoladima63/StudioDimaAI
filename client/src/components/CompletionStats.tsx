fd2440e5ab979057b93ca1fb5b99856162e7a7f7
nicoladima63 <150719135+nicoladima63@users.noreply.github.com>
2025-06-26 17:45:26 +0200

    Update CompletionStats.tsx

--- client/src/components/CompletionStats.tsx
+++ client/src/components/CompletionStats.tsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import { CCard, CCardHeader, CCardBody, CProgress } from '@coreui/react'
 
+const CompletionStats: React.FC = () => {
+  return (
+    <CCard>
+      <CCardHeader>Stato completamento</CCardHeader>
+      <CCardBody>
+        <h6>Task completati: 65%</h6>
+        <CProgress className="mb-3" color="success" value={65} />
+        <h6>Appuntamenti confermati: 80%</h6>
+        <CProgress className="mb-3" color="info" value={80} />
+        <h6>Pagamenti ricevuti: 45%</h6>
+        <CProgress className="mb-3" color="warning" value={45} />
+      </CCardBody>
+    </CCard>
+  )
+}
+
+export default CompletionStats
