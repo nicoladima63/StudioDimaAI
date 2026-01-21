674a674380e2520679288b697231ad4e4f1d3bd2
nicoladima63 <nicoladimartino@gmail.com>
2026-01-21 19:05:33 +0100

    feat: register piani_cura_v2_bp blueprint

--- server_v2/app_v2.py
+++ server_v2/app_v2.py
@@ -182,6 +182,7 @@ def register_blueprints(app: Flask) -> None:
     from api.v2_fornitori import fornitori_v2_bp
     from api.v2_pazienti import pazienti_v2_bp
     from api.v2_richiami import richiami_v2_bp
+    from api.v2_piani_cura import piani_cura_v2_bp
     from api.v2_spese_fornitori import spese_fornitori_v2_bp
     from api.v2_statistiche import statistiche_v2_bp
     from api.v2_classificazioni import classificazioni_v2_bp
@@ -211,6 +212,7 @@ def register_blueprints(app: Flask) -> None:
         fornitori_v2_bp,
         pazienti_v2_bp,
         richiami_v2_bp,
+        piani_cura_v2_bp,
         spese_fornitori_v2_bp,
         statistiche_v2_bp,
         classificazioni_v2_bp,
